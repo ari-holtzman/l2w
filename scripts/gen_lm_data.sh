5905643e16b17d88f4ab9d6afdfb71185befca0c
Ari Holtzman <20871523+ari-holtzman@users.noreply.github.com>
2018-08-09 11:27:49 -0700

    Bug fix (generating from wrong file)

--- scripts/gen_lm_data.sh
+++ scripts/gen_lm_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for input in disc_train.txt valid.txt test.txt
 do
-    python generate.py --cuda --data "$1$input" --lm "$2" --dic "$3" --gen_disc_data --beam_size 4 --out "$1$input.generated_continuation"
+    python generate.py --cuda --data "$1$input.context" --lm "$2" --dic "$3" --gen_disc_data --beam_size 4 --out "$1$input.generated_continuation"
 done
